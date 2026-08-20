a82c17fcc1a171d129665fb6a74b8c5beb6f5906
bf0880-boop <bf0880@est.ort.edu.ar>
2026-08-20 08:52:52 -0300

    exporto app

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const {Client} = pkg;
 const client = new Client(dbconfig)
 await client.connect()
 
-const result = await client.query("SELECT * FROM usuario order by id")
+const result = await client.query("SELECT * FROM usuario")
 console.log(result.rows)
 const usuario1 = result.rows[0].nombre
 console.log("usuario1:",usuario1)
@@ -16,8 +16,8 @@ await client.end()
 const app = express()
 const port = 3000;
 app.get('/',(req,res)=>res.send("Welcome " + usuario1 ))
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Local en http://localhost:${PORT}`);
-});
-//export default app;
+//const PORT = process.env.PORT || 3000;
+//app.listen(PORT, () => {
+ // console.log(`Local en http://localhost:${PORT}`);
+//});
+export default app;
